f852ba118c9dba3d102bd4d8a10e2cebcd0099e3
amannocci <adrien.mannocci@gmail.com>
2016-11-03 15:35:12 +0100

    [Fixed] Handle correctly signal shutdown
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 CLASSPATH=$(find "$LOGBULK_LIB" -name '*.jar' -printf '%p:' | sed 's/:$//')
 
 # Launch
-java -Xmx${MEM} -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory -Dconfig.file=conf/app.conf ${CONF} -server -cp ${CLASSPATH} io.vertx.core.Launcher
+exec java -Xmx${MEM} -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory -Dconfig.file=conf/app.conf ${CONF} -server -cp ${CLASSPATH} io.vertx.core.Launcher --nodaemon
