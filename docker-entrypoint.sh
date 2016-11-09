9eac301da78edc7f59f42491670975cd3e256768
amannocci <adrien.mannocci@gmail.com>
2016-11-09 12:48:21 +0100

    [Added] Switch from log4j2 to logback for logging
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 CLASSPATH=$(find "$LOGBULK_LIB" -name '*.jar' -printf '%p:' | sed 's/:$//')
 
 # Launch
-exec java -Xmx${MEM} -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory -Dconfig.file=conf/app.conf ${CONF} -server -cp ${CLASSPATH} io.vertx.core.Launcher --nodaemon
+exec java -Xmx${MEM} -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Dconfig.file=conf/app.conf ${CONF} -server -cp ${CLASSPATH} io.vertx.core.Launcher --nodaemon
---
+++ src/main/java/io/techcode/logbulk/util/JsonLayout.java
@@ -0,0 +1,57 @@
+/*
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.techcode.logbulk.util;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.CoreConstants;
+import ch.qos.logback.core.LayoutBase;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * Json layout implementation.
+ */
+public class JsonLayout extends LayoutBase<ILoggingEvent> {
+
+    @Override public String doLayout(ILoggingEvent event) {
+        JsonObject log = new JsonObject();
+        log.put("level", event.getLevel().toString());
+        log.put("thread", event.getThreadName());
+        log.put("timestamp", event.getTimeStamp());
+        if (event.getMessage().startsWith("{") && event.getMessage().endsWith("}")) {
+            log.mergeIn(new JsonObject(event.getMessage()));
+        } else {
+            log.put("message", event.getFormattedMessage());
+        }
+        if (event.getCallerData() != null && event.getCallerData().length > 0) {
+            JsonArray stacktrace = new JsonArray();
+            for (StackTraceElement el : event.getCallerData()) {
+                stacktrace.add(el.toString());
+            }
+            log.put("stacktrace", stacktrace);
+        }
+        return log.encode() + CoreConstants.LINE_SEPARATOR;
+    }
+
+}
