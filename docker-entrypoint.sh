101dab3a941ec964f49e3a96c2aa470d06b77cc1
amannocci <adrien.mannocci@gmail.com>
2016-11-12 15:29:59 +0100

    [Improved] Use a better GC
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 CLASSPATH=$(find "$LOGBULK_LIB" -name '*.jar' -printf '%p:' | sed 's/:$//')
 
 # Launch
-exec java -Xmx${MEM} -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Dconfig.file=conf/app.conf ${CONF} -server -cp ${CLASSPATH} io.vertx.core.Launcher --nodaemon
+exec java -XX:+UseG1GC -Xmx${MEM} -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Dconfig.file=conf/app.conf ${CONF} -server -cp ${CLASSPATH} io.vertx.core.Launcher --nodaemon
