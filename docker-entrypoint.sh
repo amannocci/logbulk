040a93daa3dedf27d85e66b06e72e81797729171
amannocci <adrien.mannocci@gmail.com>
2017-03-21 16:57:26 +0100

    [Added] Heap dump during crash
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,9 @@
 CLASSPATH=$(find "$LOGBULK_LIB" -name '*.jar' -printf '%p:' | sed 's/:$//')
 
 # Launch
-exec java -XX:+UseG1GC -Xmx${MEM} -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Dconfig.file=conf/app.conf ${CONF} -server -cp ${CLASSPATH} io.vertx.core.Launcher --nodaemon
+exec java -XX:+UseG1GC -Xmx${MEM} \
+  -XX:+HeapDumpOnOutOfMemoryError \
+  -XX:HeapDumpPath=conf/heapdump.hprof \
+  -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory \
+  -Dconfig.file=conf/app.conf ${CONF} \
+  -server -cp ${CLASSPATH} io.vertx.core.Launcher --nodaemon
