52c655999df75ee5acdf709f7f49550c26902e7f
amannocci <adrien.mannocci@gmail.com>
2017-04-05 21:59:48 +0100

    [Added] Script to import all missings plugins
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

---
+++ build/import-plugins.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Correct path
+cd "$(dirname "$0")"
+BASE_DIR=$PWD
+BASE_PROJECT=$(dirname "$BASE_DIR")
+BASE_DEV=$(dirname "$BASE_PROJECT")
+
+# Load common
+source ${BASE_DIR}/common.sh
+info "Loading common"
+
+# Check needed
+if [ "$(is_install git)" == "1" ]
+then
+  error "Please install git to continue"
+  exit 1
+fi
+
+# Plugins list
+source ${BASE_DIR}/list-plugins.sh
+info "Loading plugins list"
+
+# Retrieve plugins from list
+for plugin in ${plugins[@]};
+do
+  cd ${BASE_DEV}
+  if [ ! -d "./logbulk-plugin-${plugin}" ]; then
+    info "Import plugin : ${plugin}"
+    git clone https://github.com/amannocci/logbulk-plugin-${plugin}
+  fi
+done
+exit 0
